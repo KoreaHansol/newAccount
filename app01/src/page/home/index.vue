26934c13999bf758201049050708e9615dd8b026
KoreaHansol <hskimaa@hanmail.net>
2021-03-19 23:24:03 +0900

    first commit

---
+++ app01/src/page/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>INDEX</div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app01/src/store/index.js
@@ -0,0 +1,17 @@
+
+import Vuex from 'vuex'
+import Vue from 'vue'
+import _ from 'lodash'
+import 'vue-datetime/dist/vue-datetime.css'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+       
+    },
+    mutations: {
+        
+    }
+})
+export default store
